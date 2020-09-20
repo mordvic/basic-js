1ab05938f5a83c3024441b166d1d220d9905f750
Dmitriy <dmitriy.maroz@intrahealth.com>
2020-09-20 22:55:17 +0300

    feat: implement Carbon dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+/**
+ * 
+ * @param {String} value 
+ */
+module.exports = function dateSample(value) {
+  if (typeof (value) === 'string' && (value > 0 && value < MODERN_ACTIVITY) && value != undefined) {
+    let result = Math.ceil(Math.log(MODERN_ACTIVITY / value) * HALF_LIFE_PERIOD / 0.693);
+    return result;
+  }
+
+  return false;
 };
+
+// console.log(dateSample(3));
