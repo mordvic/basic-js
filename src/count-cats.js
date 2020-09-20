818b35be4e1e7f92cf91f94681ddd43254079132
Dmitriy <dmitriy.maroz@intrahealth.com>
2020-09-20 23:17:23 +0300

    feat: implement Count cats! function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+/**
+ * 
+ * @param {Array} matrixArr 
+ */
+module.exports = function countCats(matrixArr) {
+  let catValue = '^^';
+
+  return matrixArr.reduce((init, item) => {
+    return init + item.reduce((accum, value) => {
+      if (value === catValue) {
+        return ++accum;
+      } else return accum;
+    }, 0)
+  }, 0)
 };
