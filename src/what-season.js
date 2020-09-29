96c816fc0c5732a6c39ec180faa6160da695489c
Dmitriy <dmitriy.maroz@intrahealth.com>
2020-09-29 19:45:51 +0300

    feat: implement Tower of Hanoi, What season? feat: begin Transform array

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (typeof (date) == 'undefined') return 'Unable to determine the time of year!';
+  if (date instanceof Date && isNaN(date.valueOf())) throw new Error();
+
+
+  switch (date.getMonth() + 1) {
+    case 12:
+    case 1:
+    case 2:
+      return 'winter';
+      break;
+    case 3:
+    case 4:
+    case 5:
+      return 'spring';
+      break;
+    case 6:
+    case 7:
+    case 8:
+      return 'summer';
+      break;
+    case 9:
+    case 10:
+    case 11:
+      return 'autumn';
+      break;
+    default:
+      return 'Unable to determine the time of year!'
+  }
 };
