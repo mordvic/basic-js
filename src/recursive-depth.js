fb75bf0a1d2f242216302547b44c803d5afd27dc
Dmitriy <dmitriy.maroz@intrahealth.com>
2020-10-01 20:38:54 +0300

    feat: Recursive depth calculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 1;
+
+    for (let element of arr) {
+      let arrDepth = 1;
+      if (Array.isArray(element)) {
+        arrDepth += this.calculateDepth(element);
+        if (arrDepth > count) {
+          count = arrDepth;
+        } else count = count;
+      }
+
+    }
+    return count;
   }
 };
