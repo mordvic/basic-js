57e1ff5d631ce8142f8973e18c689a2908383594
Dmitriy <dmitriy.maroz@intrahealth.com>
2020-10-02 20:38:06 +0300

    feat: implement Vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,62 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(mode = true) {
+    this.mode = mode;
+  }
+
+  checkLengthOfKey(msg, key) {
+
+    while (msg.length > key.length) {
+      key += key;
+    }
+    return key;
+  }
+
+  /**
+   * 
+   * @param {String} msg 
+   * @param {String} key 
+   * @param {Boolean} mode 
+   * @param {Function} charFn 
+   */
+  processMessage(msg, key, charFn) {
+    if (typeof msg === 'undefined' || typeof key === 'undefined') {
+      throw new Error();
+    }
+
+    let index = 0;
+    let result = [];
+    let keyCode = this.checkLengthOfKey(msg, key);
+
+    for (let i = 0; i < msg.length; i++) {
+      if (/[a-zA-Z]/.test(msg[i])) {
+        result.push(charFn(
+          msg[i].toUpperCase().charCodeAt(0),
+          keyCode[index++].toUpperCase().charCodeAt(0)
+        ));
+      } else result.push(msg[i]);
+    }
+
+    if (this.mode === false) {
+      return result.reverse().join('');
+    }
+
+    return result.join('');
+  }
+
+  encrypt(msg, key) {
+    return this.processMessage(msg, key, (msgChar, keyChar) => {
+      return String.fromCharCode((msgChar + keyChar) % 26 + 65);
+    });
+  }
+
+
+  decrypt(msg, key) {
+    return this.processMessage(msg, key, (msgChar, keyChar) => {
+      return String.fromCharCode((msgChar + 26 - keyChar) % 26 + 65);
+    });
   }
 }
 
