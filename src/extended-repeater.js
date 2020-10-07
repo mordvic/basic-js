de3fbf119fb6b8d89e5de8c4c95e10a0596bbcba
Dmitriy <dmitriy.maroz@intrahealth.com>
2020-10-07 22:46:35 +0300

    feat: implement Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let res = '';
+    let newOptions = options;
+
+    if (!('separator' in newOptions)) {
+        newOptions.separator = "+";
+    }
+
+    if (!('additionSeparator' in newOptions)) {
+        newOptions.additionSeparator = "|";
+    }
+
+    if ('repeatTimes' in options && newOptions.repeatTimes === undefined) {
+        newOptions.repeatTimes = 1;
+    }
+
+    if ("additionRepeatTimes" in options && newOptions.additionRepeatTimes === undefined) {
+        newOptions.additionRepeatTimes = 1;
+    }
+
+    for (let i = 0; i < newOptions.repeatTimes; i++) {
+        res += str;
+
+        for (let j = 0; j < newOptions.additionRepeatTimes; j++) {
+            res += (j === newOptions.additionRepeatTimes - 1) ? newOptions.addition : newOptions.addition + newOptions.additionSeparator;
+        }
+        res += (i === newOptions.repeatTimes - 1) ? "" : newOptions.separator;
+
+    }
+    return res;
 };
-  
